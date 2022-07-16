4c135f0c63794cfebd80f4754d540099538c5235
ThalesVB <thalesvb@live.com>
2022-07-17 00:12:33 +0100

    BeerCSS: update to 2.1.3. Adjustments to work with this version. Bonus #1: Control Light/Dark mode Bonus #2: A better Print stylesheet.

--- assets/js/layout/default.js
+++ assets/js/layout/default.js
@@ -1,3 +1,38 @@
+---
+---
+glbUtil = {
+    store: function() {
+        window.name = JSON.stringify(glbUtil.state);
+    },
+    restore: function() {
+        if (window.name.length === 0) {
+            return;
+        } 
+        glbUtil.state = JSON.parse(window.name);
+    },
+    state: {
+        currentLightMode: null
+    }
+}
+function toggleLightMode () {
+    glbUtil.state.currentLightMode = ui("mode") == "dark" ? "light" : "dark";
+    ui("mode", glbUtil.state.currentLightMode);
+    glbUtil.store();
+}
 function works_done() {
-    document.getElementById("main").classList.add("active");
+    var mainClasses = document.getElementById("main").classList;
+    var theme = ui("theme", "{{ site.root_beer.beercss.theme }}");
+    glbUtil.restore();
+    if (!glbUtil.state.currentLightMode && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        glbUtil.state.currentLightMode = "dark";
+    }
+    if (glbUtil.state.currentLightMode) {
+        theme.then(function(){
+            ui("mode", glbUtil.state.currentLightMode);
+            mainClasses.add("active");
+        });
+    } else {
+        theme.then(function(){ mainClasses.add("active"); });
+    }
+    document.getElementById("light_mode").onclick = toggleLightMode;
 }
